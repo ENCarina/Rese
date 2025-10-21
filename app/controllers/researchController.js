271aae65e06dc18bc8bf700c5227a74f24847c59
EtelkaN <nagyetelka@ktch.hu>
2025-10-21 13:13:33 +0200

    Kész

---
+++ app/controllers/researchController.js
@@ -0,0 +1,31 @@
+const ResearchController = {
+    index:(req, res) =>{
+        res.json({
+            crud: "read",
+            name: "Nagy Etelka, II-E, 2025-10-21",
+            success: true
+        })
+    },
+    store:(req, res) =>{
+        res.json({
+            crud: "create",
+            name: "Nagy Etelka, II-E, 2025-10-21",
+            success: true
+        })
+    },
+    update:(req, res) =>{
+        res.json({
+            crud: "update",
+            name: "Nagy Etelka, II-E, 2025-10-21",
+            success: true
+        })
+    },
+    destroy:(req, res) =>{
+        res.json({
+            crud: "delete",
+            name: "Nagy Etelka, II-E, 2025-10-21",
+            success: true
+        })
+    },
+}
+export default ResearchController
